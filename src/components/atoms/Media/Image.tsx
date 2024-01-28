bc4d03cc4af590eb6e7ba9c1a0d44063ce95d12f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-01-28 21:21:13 +0200

    add image loader

--- src/components/atoms/Media/Image.tsx
+++ src/components/atoms/Media/Image.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 import { useDevice } from 'hooks/useDevice';
 
 import { useEffect, useRef, useState } from 'react';
