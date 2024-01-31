a0900ffc7d785b3a0c44633efa95bebb8b407d74
dev-cocktail <dmytro.orgish88@gmail.com>
2024-01-31 08:11:54 +0200

    wip

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN_KEY = 'alb-test-token';
+export const ACCESS_TOKEN_KEY = 'alb-bini-v1-token';
 export const CART_KEY = '-cocktail-cart';
 export const FAVORITE_KEY = '-cocktail-favorite';
 export const STORE_CODE = 'DEFAULT';
-export const BASE_URL = 'https://alb-test.com/api';
+export const BASE_URL = 'https://alberto-bini.com/api';
