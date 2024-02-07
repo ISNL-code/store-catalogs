d18a62a8b11136487842c36945275518177befba
dev-cocktail <dmytro.orgish88@gmail.com>
2024-02-07 14:30:27 +0200

    wip create alberto bini main

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN_KEY = 'alb-test-token';
 export const CART_KEY = '-cocktail-cart';
 export const FAVORITE_KEY = '-cocktail-favorite';
-export const STORE_CODE = 'DEFAULT';
-export const BASE_URL = 'https://alb-test.com/api';
+export const STORE_CODE = 'ALBERTO_BINI';
+export const BASE_URL = 'https://kremen-belts.com/api';
--- src/dataBase/USERS.js
+++ src/dataBase/USERS.js
@@ -1,13 +1,14 @@
 export const USERS_DATA = [
     {
-        emailAddress: 'dmytro.orgish88@gmail.com',
+        //alberto-bini
+        emailAddress: 'alb-admin@mail.com',
         options: { manager: true },
         contacts: {
-            emailAddress: 'dmytro.orgish88@gmail.com',
-            phone: '+380665738771',
-            viber: '+380665738771',
-            whatsapp: '+380665738771',
-            telegram: 'https://t.me/@DmytroOrgish',
+            emailAddress: 'alb-admin@mail.com',
+            phone: '+380983080085',
+            viber: '+380983080085',
+            whatsapp: '+380983080085',
+            telegram: 'Albertobiniopt',
         },
     },
 ];
