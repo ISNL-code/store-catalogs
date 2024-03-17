3268c7f29973a0ab9e2751314c96e6d08fb366c8
dev-cocktail <dmytro.orgish88@gmail.com>
2024-03-17 18:23:43 +0200

    wip

--- src/components/organisms/Cards/CatalogFavoriteCard.tsx
+++ src/components/organisms/Cards/CatalogFavoriteCard.tsx
@@ -1,5 +1,4 @@
 import { Box, IconButton, Typography, useForkRef } from '@mui/material';
-import Image from 'components/atoms/Media/Image';
 import ShareButton from 'components/molecules/ToolsButtons/ShareButton';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
