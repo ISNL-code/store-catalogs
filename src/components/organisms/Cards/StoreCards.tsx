e922e09aaeb89393871c8326b6fd92f76c857f9f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-03-18 07:53:25 +0200

    first commit

--- src/components/organisms/Cards/StoreCards.tsx
+++ src/components/organisms/Cards/StoreCards.tsx
@@ -48,13 +48,13 @@ const StoreCards = ({ data, dataFavorite, setStoreToApprove }: StoreCardsInterfa
                             sx={{ display: 'flex', width: '100%', borderBottom: '1px solid #ccc', cursor: 'pointer' }}
                         >
                             <Box sx={{ width: '70%', borderRight: '1px solid #ccc' }}>
-                                <Image width={4} height={3} imgUrl={item.mainImage} />
+                                <Image store={{}} imgUrl={item?.mainImage} ref={{}} />
                             </Box>
                             <Box
                                 p={0.5}
                                 sx={{ width: '30%', display: 'flex', alignItems: 'center', backgroundColor: 'white' }}
                             >
-                                <Image width={1} height={1} imgUrl={item?.logo?.path} />
+                                <Image store={{}} imgUrl={item?.logo?.path} ref={{}} />
                             </Box>
                         </Box>
                         <Box onClick={e => e.stopPropagation()}>
