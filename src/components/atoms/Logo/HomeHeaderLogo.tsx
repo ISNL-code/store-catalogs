73b674c77ac778906cdb9af1d8e00ab4e3bb85bf
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-05 17:25:37 +0300

    wip

--- src/components/atoms/Logo/HomeHeaderLogo.tsx
+++ src/components/atoms/Logo/HomeHeaderLogo.tsx
@@ -4,75 +4,174 @@ import { useDevice } from 'hooks/useDevice';
 const HomeHeaderLogo = () => {
     const { xs } = useDevice();
     return (
-        <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer', gap: xs ? 0.75 : 1 }}>
-            <Box sx={{ boxShadow: '0 0 4px 2px #ff0000', borderRadius: '50%', border: '2px solid #000' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer', gap: xs ? 0.25 : 0.25 }}>
+            <Box sx={{ boxShadow: '0 0 5px 1px #414141c5', borderRadius: '50%', border: '2px solid #161616' }}>
                 <Box
                     sx={{
-                        width: xs ? 40 : 48,
-                        height: xs ? 40 : 48,
+                        width: xs ? 45 : 55,
+                        height: xs ? 45 : 55,
                         borderRadius: '50%',
                         overflow: 'hidden',
                         display: 'flex',
                         alignItems: 'center',
                         position: 'relative',
-                        border: '3px solid red',
                     }}
                 >
                     <img
                         src={require('assets/img/logo.png')}
                         style={{
-                            width: xs ? 50 : 66,
-                            height: xs ? 50 : 66,
+                            width: xs ? 70 : 78,
+                            height: xs ? 70 : 78,
                             position: 'absolute',
-                            top: '49.5%',
+                            top: '50%',
                             left: '50%',
-                            transform: 'translate(-50%,-50%)',
+                            transform: 'translate(-50.5%,-51.5%)',
                         }}
                         alt="logo"
                     />
                 </Box>
             </Box>
 
-            <Box sx={{ display: 'flex', gap: 0.1, alignItems: 'center', mt: 0.5 }}>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: 'red' }}>
+            <Box
+                sx={{
+                    pt: 0.3,
+                    display: 'flex',
+                    gap: 0.15,
+                    alignItems: 'center',
+                    mt: 0.5,
+                    borderBottom: '1px solid #5e5e5e',
+                    // borderTop: '1px solid #5e5e5e',
+                    px: 0.25,
+                    // boxShadow: '0 2px 5px 1px #414141c5',
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 23 : 32,
+                        fontWeight: 600,
+                        color: '#161616',
+                    }}
+                >
                     A
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    L
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    B
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    E
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    R
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    T
-                </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
-                    O
                 </Typography>
                 <Typography
                     sx={{
-                        ml: 1,
                         fontFamily: 'Young Serif',
-                        fontSize: xs ? 25 : 32,
-                        fontWeight: 700,
-                        color: 'red',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
+                    L
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
                     }}
                 >
                     B
                 </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
+                    E
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
+                    R
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
+                    T
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
+                    O
+                </Typography>
+                <Typography
+                    sx={{
+                        ml: 0.75,
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 23 : 32,
+                        fontWeight: 600,
+                        color: '#161616',
+                    }}
+                >
+                    B
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
                     I
                 </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
                     N
                 </Typography>
-                <Typography sx={{ fontFamily: 'Young Serif', fontSize: xs ? 25 : 32, fontWeight: 700, color: '#000' }}>
+                <Typography
+                    sx={{
+                        fontFamily: 'Young Serif',
+                        fontSize: xs ? 20 : 32,
+                        fontWeight: 600,
+                        color: '#5e5e5e',
+                        textShadow:
+                            '-1px 1px 0 #000,-1px 1px 0 #000,1px -1px 0 #000,-1px -1px 0 #000,0px 1px 0 #000,0px -1px 0 #000,-1px 0px 0 #000,  1px 0px 0 #000, -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000,  0px 1px 0 #000,  0px -1px 0 #000,  -1px 0px 0 #000,  1px 0px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000,  1px -1px 0 #000,  -1px -1px 0 #000,  -1px 1px 0 #000,  --1px 1px 0 #000, 1px -1px 0 #000,  -1px -1px 0 #000',
+                    }}
+                >
                     I
                 </Typography>
             </Box>
