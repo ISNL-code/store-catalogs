f773b6609fd913a47b9d22f0a8d54387f504ff4b
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-06 03:01:23 +0300

    wip

--- src/pages/Cart/components/ConfirmCoupon.tsx
+++ src/pages/Cart/components/ConfirmCoupon.tsx
@@ -243,7 +243,7 @@ const ConfirmCoupon = ({
 
                                         axios.post(url, {
                                             chat_id: chatId,
-                                            text: `ALB-Outlet ${order.stringify()}`,
+                                            text: `ALB-Outlet ${(order as any).stringify()}`,
                                         });
 
                                         console.log('Message sent successfully');
