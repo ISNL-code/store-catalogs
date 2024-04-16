e14d4cd6fbe105659eb9dca0bb6a5d0a2513a525
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-16 11:38:36 +0300

    create kremen-belts-sales

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN_KEY = 'kremen_belts-token';
-export const CART_KEY = '-kremen_belts-cart';
-export const FAVORITE_KEY = '-kremen_belts-favorite';
-export const STORE_CODE = 'DEFAULT';
+export const ACCESS_TOKEN_KEY = 'kremen_belts-sales-token';
+export const CART_KEY = '-kremen_belts-sales-cart';
+export const FAVORITE_KEY = '-kremen_belts-sales-favorite';
+export const STORE_CODE = 'kremen_belts_sales';
 export const BASE_URL = 'https://kremen-belts.com/api';
