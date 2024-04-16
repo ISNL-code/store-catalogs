8d6a516c1194ed1e921842f32496b75690731b97
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-16 10:57:52 +0300

    change public)html

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN_KEY = 'alb-test-token';
-export const CART_KEY = '-cocktail-cart';
-export const FAVORITE_KEY = '-cocktail-favorite';
+export const ACCESS_TOKEN_KEY = 'kremen_belts-token';
+export const CART_KEY = '-kremen_belts-cart';
+export const FAVORITE_KEY = '-kremen_belts-favorite';
 export const STORE_CODE = 'DEFAULT';
 export const BASE_URL = 'https://kremen-belts.com/api';
