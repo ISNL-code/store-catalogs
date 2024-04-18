de39fd1b7968a0d5e7c7b56cc27122b2ed20d2e8
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-18 14:36:25 +0300

    token

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const ACCESS_TOKEN_KEY = 'alb-test-token';
+export const ACCESS_TOKEN_KEY = 'cocktail-token';
 export const CART_KEY = '-cocktail-cart';
 export const FAVORITE_KEY = '-cocktail-favorite';
 export const STORE_CODE = 'ALBERTO_BINI';
