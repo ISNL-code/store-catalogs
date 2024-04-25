daea368273b5a4d02b5fdcf7f671f9fa633a9796
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-25 12:49:56 +0300

    wip

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,21 +1,28 @@
+import { Box } from '@mui/material';
 import { useDevice } from 'hooks/useDevice';
 import Marquee from 'react-fast-marquee';
 const HomePage = () => {
     const { s } = useDevice();
 
     return (
-        <Marquee
-            style={{ display: 'flex' }}
-            loop={0}
-            speed={65}
-            gradient
-            gradientColor="#6666664e"
-            gradientWidth={s ? 100 : 300}
-        >
-            {[...Array(12).keys()].map(el => (
-                <img style={{ maxHeight: '100vh' }} src={require(`./images/${el + 1}.jpg`)} alt={`Loading ${el + 1}`} />
-            ))}
-        </Marquee>
+        <Box style={{ overflow: 'hidden', maxHeight: '100vh' }}>
+            <Marquee
+                style={{ display: 'flex' }}
+                loop={0}
+                speed={65}
+                gradient
+                gradientColor="#6666664e"
+                gradientWidth={s ? 100 : 300}
+            >
+                {[...Array(12).keys()].map(el => (
+                    <img
+                        style={{ maxHeight: '100vh', overflow: 'hidden' }}
+                        src={require(`./images/${el + 1}.jpg`)}
+                        alt={`Loading ${el + 1}`}
+                    />
+                ))}
+            </Marquee>
+        </Box>
     );
 };
 export default HomePage;
