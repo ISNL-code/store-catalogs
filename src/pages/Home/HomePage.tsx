3ae0a4f2dff314706ec7c77296bae436a6843354
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-25 12:29:05 +0300

    wip

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,7 @@
 import Marquee from 'react-fast-marquee';
 const HomePage = () => {
     return (
-        <Marquee style={{ display: 'flex' }} loop={0} speed={75}>
+        <Marquee style={{ display: 'flex' }} loop={0} speed={65} gradient gradientColor="#00000083" gradientWidth={200}>
             {[...Array(12).keys()].map(el => (
                 <img style={{ maxHeight: '100vh' }} src={require(`./images/${el + 1}.jpg`)} alt={`Loading ${el + 1}`} />
             ))}
