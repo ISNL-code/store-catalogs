e7656e5a35b723a7dc41c683862e8868f1e6ed55
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-25 12:36:37 +0300

    wip

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,17 @@
+import { useDevice } from 'hooks/useDevice';
 import Marquee from 'react-fast-marquee';
 const HomePage = () => {
+    const { s } = useDevice();
+
     return (
-        <Marquee style={{ display: 'flex' }} loop={0} speed={65} gradient gradientColor="#00000083" gradientWidth={50}>
+        <Marquee
+            style={{ display: 'flex' }}
+            loop={0}
+            speed={65}
+            gradient
+            gradientColor="#0000004e"
+            gradientWidth={s ? 50 : 200}
+        >
             {[...Array(12).keys()].map(el => (
                 <img style={{ maxHeight: '100vh' }} src={require(`./images/${el + 1}.jpg`)} alt={`Loading ${el + 1}`} />
             ))}
