dcef38355923f8d648528265dd02edbc60bd82e2
dev-cocktail <dmytro.orgish88@gmail.com>
2024-04-27 14:30:56 +0300

    wip

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -88,13 +88,19 @@ const HomePage = () => {
             <Grid container xs={12} mt={-4}>
                 <Typography
                     variant="h1"
-                    sx={{ color: '#fff', zIndex: 1, textShadow: '#000000 0 0 5px', fontSize: sx ? 56 : 96 }}
+                    sx={{
+                        color: '#fff',
+                        zIndex: 1,
+                        textShadow: '#000000 0 0 5px',
+                        fontSize: sx ? 56 : 96,
+                        position: 'fixed',
+                    }}
                 >
                     Elegance in Outerwear
                 </Typography>
             </Grid>
             <Grid
-                mb={sx ? 25 : 5}
+                mb={sx ? 18 : 5}
                 xs={12}
                 container
                 mt={sx ? '90vh' : 15}
