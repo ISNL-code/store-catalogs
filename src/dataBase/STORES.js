f6737357ae24628bd285397d46817f2d9527071a
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-05 00:14:02 +0300

    wip

--- src/dataBase/STORES.js
+++ src/dataBase/STORES.js
@@ -1,9 +1,9 @@
 export const STORES_DATA = [
     {
-        code: 'ALBERTO_BINI',
+        code: 'alberto_bini_europe',
         appStoreUrl: '',
         playMarketUrl: '',
-        webUrl: 'https://alberto-bini.netlify.app',
+        webUrl: 'https://albertobini-brandeurope.com',
         mainImage: require('./images/alb.jpeg'),
         productImagesOptions: { width: 6, height: 9, cropX: 0, cropY: 20 },
         descriptions: [
