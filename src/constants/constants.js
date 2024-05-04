57011228be4b4591f7469486054a3da295857e01
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-04 17:37:39 +0300

    wip

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN_KEY = '-alb-outlet-token';
-export const CART_KEY = '-alb-outlet-cart';
-export const FAVORITE_KEY = '-alb-outlet-favorite';
-export const STORE_CODE = 'ALBERTO_BINI_SALES';
+export const ACCESS_TOKEN_KEY = 'master-token';
+export const CART_KEY = '-master-cart';
+export const FAVORITE_KEY = '-master-favorite';
+export const STORE_CODE = 'ALBERTO_BINI';
 export const BASE_URL = 'https://kremen-belts.com/api';
