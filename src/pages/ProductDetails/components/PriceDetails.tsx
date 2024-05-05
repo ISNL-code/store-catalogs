a1a6542bd499324d8034325e36206e35e87e8bc7
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-05 14:28:55 +0300

    wip

--- src/pages/ProductDetails/components/PriceDetails.tsx
+++ src/pages/ProductDetails/components/PriceDetails.tsx
@@ -2,9 +2,8 @@ import DetailsSection from 'components/atoms/Sections/DetailsSection';
 import DetailsPrice from 'components/molecules/PricesComponents/DetailsPrice';
 import { useOutletContext } from 'react-router-dom';
 
-const PriceDetails = ({ productDetails, isShown }) => {
+const PriceDetails = ({ productDetails, isShown, selectedVariant }) => {
     const { string, store }: any = useOutletContext();
-
     if (isShown)
         return (
             <DetailsSection label={string?.price}>
@@ -12,7 +11,7 @@ const PriceDetails = ({ productDetails, isShown }) => {
                     currency={store?.currency}
                     price={Number(productDetails?.originalPrice)}
                     discountPrice={Number(
-                        productDetails?.price?.replace('$', '')?.replace('UAH', '')?.replace('€', '')
+                        selectedVariant?.inventory[0]?.price?.replace('$', '')?.replace('UAH', '')?.replace('€', '')
                     )}
                 />
             </DetailsSection>
