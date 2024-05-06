3285b072f0fdab1070b136fa0228928d2d38871d
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-06 11:29:47 +0300

    wip

--- src/constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/constants/stores_configs/A_DEVELOP_STORE.ts
@@ -2,6 +2,6 @@ import { ALBERTO_BINI_MAIN } from './ALBERTO_BINI_MAIN'; // eslint-disable-line
 import { KREMEN_BELTS_MAIN } from './KREMEN_BELTS_MAIN'; // eslint-disable-line
 
 export const DEVELOP_STORE = {
-    ...KREMEN_BELTS_MAIN,
+    ...ALBERTO_BINI_MAIN,
     URL: ['http://localhost:3000'],
 };
