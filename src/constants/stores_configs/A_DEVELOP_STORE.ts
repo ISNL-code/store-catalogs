3fb3a12949d05f52bbeb076fbc6024ea3cc9992b
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-06 15:26:55 +0300

    wip

--- src/constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/constants/stores_configs/A_DEVELOP_STORE.ts
@@ -2,7 +2,7 @@ import { ALBERTO_BINI_MAIN } from './ALBERTO_BINI_MAIN'; // eslint-disable-line
 import { KREMEN_BELTS_MAIN } from './KREMEN_BELTS_MAIN'; // eslint-disable-line
 
 export const DEVELOP_STORE = {
-    // ...ALBERTO_BINI_MAIN,
-    ...KREMEN_BELTS_MAIN,
+    ...ALBERTO_BINI_MAIN,
+    // ...KREMEN_BELTS_MAIN,
     URL: ['http://localhost:3000', 'https://sales-nest-dev.netlify.app'],
 };
