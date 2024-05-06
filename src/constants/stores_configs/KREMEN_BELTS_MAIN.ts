80855fff2a12d8c7e0e6abf37418caa969be70f5
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-06 17:07:11 +0300

    wip

--- src/constants/stores_configs/KREMEN_BELTS_MAIN.ts
+++ src/constants/stores_configs/KREMEN_BELTS_MAIN.ts
@@ -1,7 +1,7 @@
 import { StoreType } from 'constants/types';
 
 export const KREMEN_BELTS_MAIN = {
-    URL: ['https://kremen-belts.netlify.app', 'https://kremen-belts-main.netlify.app'],
+    URL: ['https://kremen-belts.netlify.app'],
     store_name: 'Kremen Belts',
     store_code: 'DEFAULT',
     token_key: 'kbm-tkn',
