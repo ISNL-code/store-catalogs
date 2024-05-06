2bf45e07934db438c545428d9e132e226a18203a
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-06 15:11:02 +0300

    wip

--- src/layouts/Head.tsx
+++ src/layouts/Head.tsx
@@ -17,7 +17,7 @@ const Head: React.FC = () => {
             <meta name="description" content={STORE_DESCRIPTION} />
             <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${GOOGLE_ANALYTICS_ID}`}></script>
             <script>
-                {`function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config",G-${GOOGLE_ANALYTICS_ID})`}
+                {`function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-${GOOGLE_ANALYTICS_ID}")`}
             </script>
         </Helmet>
     );
