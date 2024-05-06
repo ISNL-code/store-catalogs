4c203ba145a12709ca9befaba362a4ba4afbdfd8
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-07 01:09:14 +0300

    wip

--- src/constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/constants/stores_configs/A_DEVELOP_STORE.ts
@@ -4,9 +4,9 @@ import { KREMEN_BELTS_MAIN } from './KREMEN_BELTS_MAIN'; // eslint-disable-line
 import { KREMEN_BELTS_SALES } from './KREMEN_BELTS_SALES'; // eslint-disable-line
 
 export const DEVELOP_STORE = {
-    // ...ALBERTO_BINI_MAIN,
+    ...ALBERTO_BINI_MAIN,
     // ...ALBERTO_BINI_SALES,
     // ...KREMEN_BELTS_MAIN,
-    ...KREMEN_BELTS_SALES,
+    // ...KREMEN_BELTS_SALES,
     URL: ['http://localhost:3000'],
 };
