df272a4402c3f9db60414f8f35774462f7a6d6ee
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-07 14:59:30 +0300

    wip

---
+++ src/constants/stores_configs/ALBERTO_BINI_OUTLET.ts
@@ -0,0 +1,28 @@
+import { StoreType } from 'constants/types';
+
+const prefix = 'abo';
+
+export const ALBERTO_BINI_OUTLET = {
+    URL: ['https://alberto-bini-outlet.netlify.app'],
+    store_name: 'Alberto Bini Outlet',
+    store_code: 'ALBERTO_BINI_SALES',
+    token_key: `${prefix}-tkn`,
+    cart_key: `${prefix}-cart`,
+    favorite_url: `${prefix}-favorites`,
+    base_url: 'https://kremen-belts.com/api',
+    lang_key: `${prefix}-lang`,
+    app_lang: 'ua',
+    options: {
+        main_price_multiplication: 3,
+        sale_price_multiplication: 2,
+        currency_multiplication: 40,
+        custom_currency: '₴',
+        store_type: StoreType?.sales,
+    },
+    web_head_data: {
+        store_title: 'Alberto Bini Каталог | Alberto Bini Outlet Catalog ',
+        store_description: `Ласкаво просимо до Alberto Bini — світу вишуканого жіночого верхнього одягу! Дослідіть наш широкий вибір високоякісних стильних пальт, тренчів та курток. Погрузіться в унікальний стиль та комфорт з Alberto Bini!  Welcome to Alberto Bini — the world of exquisite women's outerwear! Explore our wide selection of high-quality stylish coats, trenches, and jackets. Immerse yourself in unique style and comfort with Alberto Bini!`,
+        google_analytics_id: 'G-C4TDRX7K5V',
+        store_logo: 'alberto_bini_logo.png',
+    },
+};
