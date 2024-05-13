46db3633435175b2f0687efb46d0131fc64d0eb2
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-13 21:45:02 +0300

    wip

--- src/constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/constants/stores_configs/A_DEVELOP_STORE.ts
@@ -10,8 +10,8 @@ import { ALBERTO_BINI_KZ_RETAIL } from './ALBERTO_BINI_KZ_RETAIL'; // eslint-dis
 
 export const DEVELOP_STORE = {
     // ...ALBERTO_BINI_MAIN,
-    ...ALBERTO_BINI_EU_WS,
-    // ...ALBERTO_BINI_EU_RETAIL,
+    // ...ALBERTO_BINI_EU_WS,
+    ...ALBERTO_BINI_EU_RETAIL,
     // ...ALBERTO_BINI_KZ_RETAIL,
     // ...ALBERTO_BINI_KZ_WS,
     // ...ALBERTO_BINI_OUTLET,
