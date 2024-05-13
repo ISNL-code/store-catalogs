663c44fbf1992d431bc5996bd75bc4ae4775d2ba
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-13 16:08:17 +0300

    wip

--- src/components/atoms/Buttons/SideLink.tsx
+++ src/components/atoms/Buttons/SideLink.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import LinkIcon from '@mui/icons-material/Link';
 import { Colors } from 'colors';
 import { useDevice } from 'hooks/useDevice';
