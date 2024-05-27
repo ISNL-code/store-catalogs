38d2af178a3e24cf2cd8dcb1a0f9083ac6a4d5e0
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-27 14:25:34 +0300

    wip

--- src/layouts/Head_Landing_HTML.tsx
+++ src/layouts/Head_Landing_HTML.tsx
@@ -60,14 +60,62 @@ const Head_Landing_HTML: React.FC = () => {
                 <html lang="en" />
                 <meta name="theme-color" content="#000000" />
                 <title>Sales Nest</title>
-                <meta name="description" content={STORE_DESCRIPTION} />
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}></script>
-                <script>
-                    {`function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GOOGLE_ANALYTICS_ID}")`}
-                </script>
-                <link rel="icon" href={require(`dataBase/images/logos/${STORE_LOGO}`)} />
-                <link rel="apple-touch-icon" href={require(`dataBase/images/logos/${STORE_LOGO}`)} />
+                <meta
+                    name="description"
+                    content="Online product catalog is a powerful software solution for creating and managing electronic catalogs. Convenient addition and classification of products, product visualization for online stores and exhibitions. | Онлайн каталог товаров – мощное программное решение для создания и управления электронными каталогами. Удобное добавление и классификация товаров, визуализация продукции для интернет-магазинов и выставок. | Онлайн каталог товарів – потужне програмне рішення для створення та керування електронними каталогами. Зручне додавання та класифікація товарів, візуалізація продукції для інтернет-магазинів та виставок."
+                />
+                <meta property="og:title" content="Sales Nest Online Catalog | Заказать онлайн каталог товаров" />
+                <meta
+                    property="og:description"
+                    content="Online product catalog is a powerful software solution for creating and managing electronic catalogs. Convenient addition and classification of products, product visualization for online stores and exhibitions. | Онлайн каталог товаров – мощное программное решение для создания и управления электронными каталогами. Удобное добавление и классификация товаров, визуализация продукции для интернет-магазинов и выставок. | Онлайн каталог товарів – потужне програмне рішення для створення та керування електронними каталогами. Зручне додавання та класифікація товарів, візуалізація продукції для інтернет-магазинів та виставок."
+                />
+                <link rel="icon" href={require('assets/img/logo.png')} />
+                <link rel="apple-touch-icon" href={require('assets/img/logo.png')} />
                 <link rel="manifest" href={manifestUrl} />
+                <meta
+                    name="keywords"
+                    content="интернет-каталог товаров, создание электронных каталогов, описание товаров, добавление товаров, классификация товаров, online product catalog, electronic catalog creation, product descriptions, product listing, product classification, інтернет-каталог товарів, створення електронних каталогів, опис товарів, додавання товарів, класифікація товарів"
+                />
+                <link rel="canonical" href="https://salesnestonlinecatalog.com" />
+
+                {/* <!-- Google analytics TAG --> */}
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=G-5EKVQYRR8P`}></script>
+                <script>
+                    {`function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-5EKVQYRR8P")`}
+                </script>
+
+                {/* <!-- Event snippet for Покупка conversion page --> */}
+                <script>
+                    <script>
+                        {`gtag('event', 'conversion', {
+                        send_to: 'AW-16532045891/BjdfCLir_qYZEMOAjMs9',
+                        value: 1.0,
+                        currency: 'UAH',
+                        transaction_id: '',
+                    });`}
+                    </script>
+                </script>
+
+                {/*    <!-- Event snippet for Покупка conversion page
+        In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. --> */}
+
+                <script>
+                    {`function gtag_report_conversion(url) {
+                        var callback = function () {
+                            if (typeof url != 'undefined') {
+                                window.location = url;
+                            }
+                        };
+                        gtag('event', 'conversion', {
+                            send_to: 'AW-16532045891/BjdfCLir_qYZEMOAjMs9',
+                            value: 1.0,
+                            currency: 'UAH',
+                            transaction_id: '',
+                            event_callback: callback,
+                        });
+                        return false;
+                    }`}
+                </script>
             </Helmet>
         </HelmetProvider>
     );
