8a76027b0998ab52a2dd0a7388a25a625ef15a9d
dev-cocktail <dmytro.orgish88@gmail.com>
2024-05-29 06:07:44 +0300

    create store email is required field

--- src/utils/shareFunctions.ts
+++ src/utils/shareFunctions.ts
@@ -6,28 +6,26 @@ const openInNewWindow = (url: string) => {
 };
 
 export const shareOnTelegram = (url: string, imagePath: string) => {
-    const message = 'Check this out: '; // Your message goes here
-    const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(
-        `${message}${url} ${imagePath}`
-    )}`;
+    const message = `Check this out: ${url}`;
+    const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(message)}`;
     openInNewWindow(telegramUrl);
 };
 
 export const shareOnWhatsApp = (url: string, imagePath: string) => {
-    const message = 'Check this out: '; // Your message goes here
-    const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(`${message}${url} ${imagePath}`)}`;
+    const message = `Check this out: ${url}`;
+    const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}%0A${encodeURIComponent(imagePath)}`;
     openInNewWindow(whatsappUrl);
 };
 
 export const shareOnViber = (url: string, imagePath: string) => {
-    const message = 'Check this out: '; // Your message goes here
-    const viberUrl = `viber://forward?text=${encodeURIComponent(`${message}${url} ${imagePath}`)}`;
+    const message = `Check this out: ${url}`;
+    const viberUrl = `viber://forward?text=${encodeURIComponent(message)}%0A${encodeURIComponent(imagePath)}`;
     openInNewWindow(viberUrl);
 };
 
 export const shareOnEmail = (url: string, imagePath: string) => {
-    const subject = 'Interesting link'; // Your subject goes here
-    const body = `I thought you might find this interesting:\n\n${url}\n\nImage: ${imagePath}`; // Your email body goes here
+    const subject = 'Interesting link';
+    const body = `I thought you might find this interesting:\n\n${url}\n\nImage: ${imagePath}`;
     const mailtoUrl = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
     openInNewWindow(mailtoUrl);
 };
