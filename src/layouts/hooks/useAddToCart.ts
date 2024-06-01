8d236e85406d24afe6db34d41684b50073b4462f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-02 00:39:43 +0300

    wip

--- src/layouts/hooks/useAddToCart.ts
+++ src/layouts/hooks/useAddToCart.ts
@@ -1,29 +1,29 @@
-import { useIsMount } from 'hooks/useIsMount';
-import { useEffect, useState } from 'react';
+// import { useIsMount } from 'hooks/useIsMount';
+import { useState } from 'react';
 import { LocalStorageProductInterface, useAddToCartDataInterface } from 'types/app_models';
-import { STORAGE_KEYS } from 'constants/local_storage_keys';
+// import { STORAGE_KEYS } from 'constants/local_storage_keys';
 
 interface useAddToCartParamsInterface {
     loadingUser: boolean;
 }
 
 export const useAddToCart = ({ loadingUser }: useAddToCartParamsInterface): useAddToCartDataInterface => {
-    const mount = useIsMount();
+    // const mount = useIsMount();
     const [cartItems, setCartItems] = useState<LocalStorageProductInterface[]>([]);
 
-    useEffect(() => {
-        if (loadingUser) return;
-        setCartItems(JSON.parse(localStorage.getItem(STORAGE_KEYS?.CART_KEY) as string) || []); // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // useEffect(() => {
+    //     if (loadingUser) return;
+    //     setCartItems(JSON.parse(localStorage.getItem(STORAGE_KEYS?.CART_KEY) as string) || []); // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
-    useEffect(() => {
-        if (loadingUser) return;
+    // useEffect(() => {
+    //     if (loadingUser) return;
 
-        if (mount) return;
-        if (cartItems.length) {
-            localStorage.setItem(STORAGE_KEYS?.CART_KEY, JSON.stringify(cartItems));
-        } else localStorage.removeItem(STORAGE_KEYS?.CART_KEY); // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cartItems]);
+    //     if (mount) return;
+    //     if (cartItems.length) {
+    //         localStorage.setItem(STORAGE_KEYS?.CART_KEY, JSON.stringify(cartItems));
+    //     } else localStorage.removeItem(STORAGE_KEYS?.CART_KEY); // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [cartItems]);
 
     const handleSetCartItems = (data: LocalStorageProductInterface) => {
         if (cartItems?.find(item => item?.variantSku === data?.variantSku)) {
--- src/layouts/hooks/useAddToFavorites.tsx
+++ src/layouts/hooks/useAddToFavorites.tsx
@@ -1,28 +1,28 @@
-import { useIsMount } from 'hooks/useIsMount';
-import { useEffect, useState } from 'react';
+// import { useIsMount } from 'hooks/useIsMount';
+import { useState } from 'react';
 import { LocalStorageProductInterface, useAddToFavoriteDataInterface } from 'types/app_models';
-import { STORAGE_KEYS } from 'constants/local_storage_keys';
+// import { STORAGE_KEYS } from 'constants/local_storage_keys';
 
 interface useAddToFavoritesParamsInterface {
     loadingUser: boolean;
 }
 
 export const useAddToFavorites = ({ loadingUser }: useAddToFavoritesParamsInterface): useAddToFavoriteDataInterface => {
-    const mount = useIsMount();
+    // const mount = useIsMount();
     const [favoriteItems, setFavoriteItems] = useState<LocalStorageProductInterface[]>([]);
 
-    useEffect(() => {
-        if (loadingUser) return;
-        setFavoriteItems(JSON.parse(localStorage.getItem(STORAGE_KEYS?.FAVORITE_KEY) as string) || []); // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // useEffect(() => {
+    //     if (loadingUser) return;
+    //     setFavoriteItems(JSON.parse(localStorage.getItem(STORAGE_KEYS?.FAVORITE_KEY) as string) || []); // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
-    useEffect(() => {
-        if (loadingUser) return;
-        if (mount) return;
-        if (favoriteItems.length) {
-            localStorage.setItem(STORAGE_KEYS?.FAVORITE_KEY, JSON.stringify(favoriteItems));
-        } else localStorage.removeItem(STORAGE_KEYS?.FAVORITE_KEY); // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [favoriteItems]);
+    // useEffect(() => {
+    //     if (loadingUser) return;
+    //     if (mount) return;
+    //     if (favoriteItems.length) {
+    //         localStorage.setItem(STORAGE_KEYS?.FAVORITE_KEY, JSON.stringify(favoriteItems));
+    //     } else localStorage.removeItem(STORAGE_KEYS?.FAVORITE_KEY); // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [favoriteItems]);
 
     const handleSetFavoriteItems = (data: LocalStorageProductInterface) => {
         if (favoriteItems?.find(item => item?.variantSku === data?.variantSku)) {
