1d3d7a3c74dbc1c0f0b87adf73f5684509037d2f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-03 14:52:15 +0300

    wip

--- src/components/molecules/ToolsButtons/ShareButton.tsx
+++ src/components/molecules/ToolsButtons/ShareButton.tsx
@@ -60,9 +60,8 @@ const ShareButton = ({ path, isShown, direction, size }: ShareButtonInterface) =
                     onOpen={() => {
                         if ('share' in navigator) {
                             navigator.share({
-                                title: 'Alberto Bini',
-                                text: 'Model',
-                                url: `https://sales-nest.netlify.app/store/alberto_bini_europe/product/1419/model/201-0622k`,
+                                url: path,
+                                files: undefined,
                             });
                             return;
                         }
