8f24f1dd7102f7d0d0a4187d3ccdfc31b836bd84
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-03 16:25:36 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,27 @@ import StoresRouting from 'router/StoresRouting';
 import { Toaster } from 'react-hot-toast';
 import { useDevice } from 'hooks/useDevice';
 import HeadStoresHTML from 'layouts/Head-Stores-HTML';
+import { useEffect } from 'react';
 
 const App = () => {
     const { sx } = useDevice();
+    useEffect(() => {
+        function handleNavigation(event) {
+            // Проверяем, является ли событие навигацией
+            if (event.type === 'popstate') {
+                // Ваш код здесь, например, вывод алерта
+                alert('Была выполнена навигация браузера!');
+            }
+        }
+
+        // Добавляем слушатели событий навигации
+        window.addEventListener('popstate', handleNavigation);
+
+        // Очистка слушателей при размонтировании компонента
+        return () => {
+            window.removeEventListener('popstate', handleNavigation);
+        };
+    }, []); // Пустой массив зависимостей означает, что эффект будет запущен только после первого рендера
 
     return (
         <ThemeProvider theme={mainTheme}>
--- src/components/atoms/Buttons/SaveToHomeScreen.tsx
+++ src/components/atoms/Buttons/SaveToHomeScreen.tsx
@@ -5,24 +5,6 @@ import { useDevice } from 'hooks/useDevice';
 const SaveToHomeScreen = () => {
     const { sx } = useDevice();
 
-    const showAddToHomeScreenPrompt = () => {
-        if ('share' in navigator && window.matchMedia('(display-mode: standalone)').matches) {
-            // Show a prompt to add to home screen
-            navigator
-                .share({
-                    title: 'Добавить на домашний экран',
-                    text: 'Установите это приложение на ваш домашний экран для легкого доступа.',
-                    url: window.location.href,
-                })
-                .then(() => {
-                    console.log('Приложение успешно добавлено на главный экран');
-                })
-                .catch(error => {
-                    alert(error);
-                });
-        }
-    };
-
     return (
         <>
             <Fab
@@ -34,7 +16,6 @@ const SaveToHomeScreen = () => {
                     bottom: sx ? 80 : 16,
                     backgroundColor: '#ffffffbe',
                 }}
-                onClick={showAddToHomeScreenPrompt}
             >
                 <AppleIcon />
             </Fab>
