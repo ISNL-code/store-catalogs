e7a761fdea6bbff743fb78c7bf6ac77c4592cb12
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-03 21:07:54 +0300

    wip

--- src/components/atoms/Buttons/SaveToHomeScreen.tsx
+++ src/components/atoms/Buttons/SaveToHomeScreen.tsx
@@ -1,11 +1,9 @@
-import { Fab, Modal, Typography, Button } from '@mui/material';
-import { useState } from 'react';
+import { Fab } from '@mui/material';
 import { AppleIcon } from 'assets/svg/apple_icon';
 import { useDevice } from 'hooks/useDevice';
 
 const SaveToHomeScreen = () => {
     const { sx } = useDevice();
-    const [modalOpen, setModalOpen] = useState(false);
 
     function addToHomeScreen() {
         if (navigator && navigator['standalone']) {
@@ -15,46 +13,24 @@ const SaveToHomeScreen = () => {
         } else if (window.navigator['standalone'] === undefined) {
             alert('Пожалуйста, добавьте это приложение на главный экран вашего устройства.');
         } else {
-            setModalOpen(true);
+            alert('Нажмите кнопку "Поделиться" и выберите "Добавить на главный экран".');
         }
     }
 
     return (
-        <>
-            <Fab
-                size="medium"
-                sx={{
-                    zIndex: 50,
-                    position: 'fixed',
-                    left: sx ? '80px' : '40px',
-                    bottom: sx ? 80 : 16,
-                    backgroundColor: '#ffffffbe',
-                }}
-                onClick={addToHomeScreen}
-            >
-                <AppleIcon />
-            </Fab>
-            <Modal
-                open={modalOpen}
-                onClose={() => setModalOpen(false)}
-                aria-labelledby="instruction-modal"
-                aria-describedby="instruction-modal-description"
-            >
-                <div>
-                    <Typography id="instruction-modal" variant="h6">
-                        Как добавить приложение на главный экран
-                    </Typography>
-                    <Typography id="instruction-modal-description" sx={{ mt: 2 }}>
-                        Инструкции по добавлению вашего приложения на главный экран могут различаться в зависимости от
-                        браузера и операционной системы вашего устройства. Пожалуйста, следуйте инструкциям, чтобы
-                        добавить это приложение на главный экран вашего устройства.
-                    </Typography>
-                    <Button sx={{ mt: 2 }} onClick={() => setModalOpen(false)}>
-                        Закрыть
-                    </Button>
-                </div>
-            </Modal>
-        </>
+        <Fab
+            size="medium"
+            sx={{
+                zIndex: 50,
+                position: 'fixed',
+                left: sx ? '80px' : '40px',
+                bottom: sx ? 80 : 16,
+                backgroundColor: '#ffffffbe',
+            }}
+            onClick={addToHomeScreen}
+        >
+            <AppleIcon />
+        </Fab>
     );
 };
 
