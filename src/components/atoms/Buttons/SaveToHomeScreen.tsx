387dc09514b5f6fd292139c07e63b3d803ba7f44
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-03 19:40:48 +0300

    wip

--- src/components/atoms/Buttons/SaveToHomeScreen.tsx
+++ src/components/atoms/Buttons/SaveToHomeScreen.tsx
@@ -6,30 +6,33 @@ const SaveToHomeScreen = () => {
     const { sx } = useDevice();
 
     const showAddToHomeScreenPrompt = () => {
-        if ('share' in navigator) {
-            // Show a prompt to add to home screen
-            navigator.share({
-                url: window.location.href,
-            });
+        if (navigator.share) {
+            navigator
+                .share({
+                    title: document.title,
+                    text: 'Add this app to your home screen!',
+                    url: window.location.href,
+                })
+                .catch(error => console.log('Error sharing', error));
+        } else {
+            alert('Your browser does not support the Share API.');
         }
     };
 
     return (
-        <>
-            <Fab
-                size="medium"
-                sx={{
-                    zIndex: 50,
-                    position: 'fixed',
-                    left: sx ? '80px' : '40px',
-                    bottom: sx ? 80 : 16,
-                    backgroundColor: '#ffffffbe',
-                }}
-                onClick={showAddToHomeScreenPrompt}
-            >
-                <AppleIcon />
-            </Fab>
-        </>
+        <Fab
+            size="medium"
+            sx={{
+                zIndex: 50,
+                position: 'fixed',
+                left: sx ? '80px' : '40px',
+                bottom: sx ? 80 : 16,
+                backgroundColor: '#ffffffbe',
+            }}
+            onClick={showAddToHomeScreenPrompt}
+        >
+            <AppleIcon />
+        </Fab>
     );
 };
 
