f17ecc6a36ba8fb957796a37498f13f516bed2c1
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-04 03:04:45 +0300

    wip

--- src/store_constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/store_constants/stores_configs/A_DEVELOP_STORE.ts
@@ -24,6 +24,6 @@ export const DEVELOP_STORE = {
         'https://sales-nest-dev.netlify.app',
         'https://sales-nest.netlify.app',
     ],
-    telegram_sender: !window?.location?.origin.includes('localhost'),
+    // telegram_sender: !window?.location?.origin.includes('localhost'),
     // telegram_sender: true,
 };
