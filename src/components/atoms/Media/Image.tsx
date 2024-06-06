8fa55d8bbf6465ddffdf12b5aac3fae521bcf68f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-06 14:40:09 +0300

    wip

--- src/components/atoms/Media/Image.tsx
+++ src/components/atoms/Media/Image.tsx
@@ -1,22 +1,22 @@
 import { CircularProgress } from '@mui/material';
 import { Color } from 'constants/colors';
 import { useState } from 'react';
+import CardMedia from '@mui/material/CardMedia';
 
 const ImageComponent = ({ imageUrl }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
 
     return (
         <div style={{ height: '100%', position: 'relative', width: '100%' }}>
-            <img
-                src={imageUrl}
-                style={{
-                    width: '100%',
-                    opacity: imgLoaded ? 1 : 0,
-                }}
-                alt="Loading..."
+            <CardMedia
+                component="img"
+                height="fit-content"
+                image={imageUrl}
+                alt="image"
                 onLoad={event => {
                     setImgLoaded(!event?.bubbles);
                 }}
+                sx={{ opacity: imgLoaded ? 1 : 0 }}
             />
             <div style={{ left: '50%', top: '50%', transform: 'translate(-50%,-50%)', position: 'absolute' }}>
                 <CircularProgress
