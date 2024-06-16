bf38528d2a6068f9073cf0b909297db45d25a40d
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-16 17:24:40 +0300

    wip

--- src/components/organisms/Modals/TableSizeDialog.tsx
+++ src/components/organisms/Modals/TableSizeDialog.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Button, DialogActions, Dialog, ClickAwayListener } from '@mui/material';
+import { Button, DialogActions, Dialog } from '@mui/material';
 import { Colors } from 'constants/colors';
 import DialogContent from '@mui/material/DialogContent';
 import { useDevice } from 'hooks/useDevice';
@@ -29,57 +29,49 @@ const TableSizeDialog = ({ string, onClose, closeAvailable, onSubmit, dialogStat
     }, []);
 
     return (
-        <ClickAwayListener
-            onClickAway={() => {
-                if (closeAvailable) handleClose();
-            }}
-            mouseEvent={false}
-            touchEvent={false}
-        >
-            <>
-                {!imgLoaded && <Loader />}
-                <Dialog
-                    sx={{ zIndex: 4500, width: '100vw', opacity: imgLoaded ? 1 : 0 }}
-                    BackdropProps={{ style: { zIndex: 5000 } }}
-                    open={open}
-                    onClose={() => {
-                        if (closeAvailable) handleClose();
-                    }}
-                    hideBackdrop
-                    PaperProps={{
-                        component: 'form',
-                        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-                            event.preventDefault();
-                            onSubmit();
-                        },
-                        sx: {
-                            borderRadius: 4,
-                            overflow: 'hidden',
-                            border: `0.5px solid ${Colors?.GRAY_300}`,
-                            width: 'fit-content',
-                            maxWidth: '98vw',
-                            m: 0,
-                        },
-                    }}
-                >
-                    <DialogContent sx={{ p: 0 }}>
-                        <img
-                            style={{ width: 'auto', maxHeight: sx ? '55vh' : '75vh', maxWidth: '100%' }}
-                            src={dialogState?.imageUrl}
-                            alt="Loading..."
-                            onLoad={event => {
-                                setImgLoaded(!event?.bubbles);
-                            }}
-                        />
-                    </DialogContent>
-                    <DialogActions sx={{ py: 2, my: 0, px: 3, flexWrap: 'wrap' }}>
-                        <Button variant="contained" type="submit">
-                            {string?.close}
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </>
-        </ClickAwayListener>
+        <>
+            {!imgLoaded && <Loader />}
+            <Dialog
+                sx={{ zIndex: 4500, width: '100vw', opacity: imgLoaded ? 1 : 0 }}
+                BackdropProps={{ style: { zIndex: 5000 } }}
+                open={open}
+                onClose={() => {
+                    if (closeAvailable) handleClose();
+                }}
+                hideBackdrop
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                        event.preventDefault();
+                        onSubmit();
+                    },
+                    sx: {
+                        borderRadius: 4,
+                        overflow: 'hidden',
+                        border: `0.5px solid ${Colors?.GRAY_300}`,
+                        width: 'fit-content',
+                        maxWidth: '98vw',
+                        m: 0,
+                    },
+                }}
+            >
+                <DialogContent sx={{ p: 0 }}>
+                    <img
+                        style={{ width: 'auto', maxHeight: sx ? '55vh' : '75vh', maxWidth: '100%' }}
+                        src={dialogState?.imageUrl}
+                        alt="Loading..."
+                        onLoad={event => {
+                            setImgLoaded(!event?.bubbles);
+                        }}
+                    />
+                </DialogContent>
+                <DialogActions sx={{ py: 2, my: 0, px: 3, flexWrap: 'wrap' }}>
+                    <Button variant="contained" type="submit">
+                        {string?.close}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
