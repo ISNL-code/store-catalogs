b1782c23d5813b9c1613b578677f46dd5a73c442
dev-cocktail <dmytro.orgish88@gmail.com>
2024-06-16 17:14:29 +0300

    wip

--- src/components/organisms/Modals/TableSizeDialog.tsx
+++ src/components/organisms/Modals/TableSizeDialog.tsx
@@ -4,6 +4,7 @@ import { Colors } from 'constants/colors';
 import DialogContent from '@mui/material/DialogContent';
 import { useDevice } from 'hooks/useDevice';
 import { DialogStateInterface } from 'types/app_models';
+import Loader from 'components/atoms/Loader/Loader';
 
 interface Props {
     string; // Assuming `string` is used for the close button text
@@ -35,46 +36,49 @@ const TableSizeDialog = ({ string, onClose, closeAvailable, onSubmit, dialogStat
             mouseEvent={false}
             touchEvent={false}
         >
-            <Dialog
-                sx={{ zIndex: 4500, width: '100vw', opacity: imgLoaded ? 1 : 0 }}
-                BackdropProps={{ style: { zIndex: 5000 } }}
-                open={open}
-                onClose={() => {
-                    if (closeAvailable) handleClose();
-                }}
-                hideBackdrop
-                PaperProps={{
-                    component: 'form',
-                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-                        event.preventDefault();
-                        onSubmit();
-                    },
-                    sx: {
-                        borderRadius: 4,
-                        overflow: 'hidden',
-                        border: `0.5px solid ${Colors?.GRAY_300}`,
-                        width: 'fit-content',
-                        maxWidth: '98vw',
-                        m: 0,
-                    },
-                }}
-            >
-                <DialogContent sx={{ p: 0 }}>
-                    <img
-                        style={{ width: 'auto', maxHeight: sx ? '55vh' : '75vh', maxWidth: '100%' }}
-                        src={dialogState?.imageUrl}
-                        alt="Loading..."
-                        onLoad={event => {
-                            setImgLoaded(!event?.bubbles);
-                        }}
-                    />
-                </DialogContent>
-                <DialogActions sx={{ py: 2, my: 0, px: 3, flexWrap: 'wrap' }}>
-                    <Button variant="contained" type="submit">
-                        {string?.close}
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <>
+                {!imgLoaded && <Loader />}
+                <Dialog
+                    sx={{ zIndex: 4500, width: '100vw', opacity: imgLoaded ? 1 : 0 }}
+                    BackdropProps={{ style: { zIndex: 5000 } }}
+                    open={open}
+                    onClose={() => {
+                        if (closeAvailable) handleClose();
+                    }}
+                    hideBackdrop
+                    PaperProps={{
+                        component: 'form',
+                        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                            event.preventDefault();
+                            onSubmit();
+                        },
+                        sx: {
+                            borderRadius: 4,
+                            overflow: 'hidden',
+                            border: `0.5px solid ${Colors?.GRAY_300}`,
+                            width: 'fit-content',
+                            maxWidth: '98vw',
+                            m: 0,
+                        },
+                    }}
+                >
+                    <DialogContent sx={{ p: 0 }}>
+                        <img
+                            style={{ width: 'auto', maxHeight: sx ? '55vh' : '75vh', maxWidth: '100%' }}
+                            src={dialogState?.imageUrl}
+                            alt="Loading..."
+                            onLoad={event => {
+                                setImgLoaded(!event?.bubbles);
+                            }}
+                        />
+                    </DialogContent>
+                    <DialogActions sx={{ py: 2, my: 0, px: 3, flexWrap: 'wrap' }}>
+                        <Button variant="contained" type="submit">
+                            {string?.close}
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </>
         </ClickAwayListener>
     );
 };
