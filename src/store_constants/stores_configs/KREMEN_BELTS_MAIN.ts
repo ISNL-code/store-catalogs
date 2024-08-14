099e558561a78d1cfea634c61e97556e372d9b23
dev-cocktail <dmytro.orgish88@gmail.com>
2024-08-14 11:00:16 +0300

    wip

--- src/store_constants/stores_configs/KREMEN_BELTS_MAIN.ts
+++ src/store_constants/stores_configs/KREMEN_BELTS_MAIN.ts
@@ -2,7 +2,7 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const KREMEN_BELTS_MAIN = {
     storage_key: 'kb_ua-ws',
-    active: true,
+    active: false,
     required_registration: false,
     URL: ['https://kremen-belts.netlify.app'],
     web_lang: 'uk', // html lang
--- src/store_constants/stores_configs/KREMEN_BELTS_SALES.ts
+++ src/store_constants/stores_configs/KREMEN_BELTS_SALES.ts
@@ -2,7 +2,7 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const KREMEN_BELTS_SALES = {
     storage_key: 'kb_ua-wss',
-    active: true,
+    active: false,
     required_registration: false,
     URL: ['https://kremen-belts-sales.netlify.app'],
     web_lang: 'uk', // html lang
