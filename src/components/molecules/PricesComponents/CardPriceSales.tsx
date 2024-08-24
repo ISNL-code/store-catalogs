88259c4e51786852ade2f601f0f2da42d93e6b6d
dev-cocktail <dmytro.orgish88@gmail.com>
2024-08-24 22:15:59 +0300

    wip

--- src/components/molecules/PricesComponents/CardPriceSales.tsx
+++ src/components/molecules/PricesComponents/CardPriceSales.tsx
@@ -21,37 +21,55 @@ const CardPriceSales = ({ currency, originalPrice, discountPrice }: Props) => {
 
     return (
         <>
-            <Box sx={{ display: 'flex', gap: 0.1, alignItems: 'center' }}>
-                {discountPrice && (
-                    <Typography sx={{ color: Color.ERROR, fontSize: 18, fontWeight: 700 }}>
-                        {CUSTOM_CURRENCY || map_currency_symbol(currency)}
-                        {parseFloat(
-                            (
-                                discountPrice *
-                                SALE_PRICE_MULTIPLICATION *
-                                CURRENCY_MULTIPLICATION *
-                                RETAIL_PRICE_MULTIPLICATION
-                            ).toFixed(2)
-                        )}
-                    </Typography>
-                )}
-                <Typography sx={{ color: Colors?.GRAY_900, fontSize: 15, textDecoration: 'line-through' }}>
-                    /
-                </Typography>
-                {originalPrice && (
+            {originalPrice !== discountPrice ? (
+                <Box sx={{ display: 'flex', gap: 0.1, alignItems: 'center' }}>
+                    {discountPrice && (
+                        <Typography sx={{ color: Color.ERROR, fontSize: 18, fontWeight: 700 }}>
+                            {CUSTOM_CURRENCY || map_currency_symbol(currency)}
+                            {parseFloat(
+                                (
+                                    discountPrice *
+                                    SALE_PRICE_MULTIPLICATION *
+                                    CURRENCY_MULTIPLICATION *
+                                    RETAIL_PRICE_MULTIPLICATION
+                                ).toFixed(2)
+                            )}
+                        </Typography>
+                    )}
                     <Typography sx={{ color: Colors?.GRAY_900, fontSize: 15, textDecoration: 'line-through' }}>
-                        {CUSTOM_CURRENCY || map_currency_symbol(currency)}
-                        {parseFloat(
-                            (
-                                originalPrice *
-                                RETAIL_PRICE_MULTIPLICATION *
-                                CURRENCY_MULTIPLICATION *
-                                MAIN_PRICE_MULTIPLICATION
-                            ).toFixed(2)
-                        )}
+                        /
                     </Typography>
-                )}
-            </Box>
+                    {originalPrice && (
+                        <Typography sx={{ color: Colors?.GRAY_900, fontSize: 15, textDecoration: 'line-through' }}>
+                            {CUSTOM_CURRENCY || map_currency_symbol(currency)}
+                            {parseFloat(
+                                (
+                                    originalPrice *
+                                    RETAIL_PRICE_MULTIPLICATION *
+                                    CURRENCY_MULTIPLICATION *
+                                    MAIN_PRICE_MULTIPLICATION
+                                ).toFixed(2)
+                            )}
+                        </Typography>
+                    )}
+                </Box>
+            ) : (
+                <>
+                    {originalPrice && (
+                        <Typography sx={{ color: Colors?.GRAY_900, fontSize: 18 }}>
+                            {CUSTOM_CURRENCY || map_currency_symbol(currency)}
+                            {parseFloat(
+                                (
+                                    originalPrice *
+                                    RETAIL_PRICE_MULTIPLICATION *
+                                    CURRENCY_MULTIPLICATION *
+                                    MAIN_PRICE_MULTIPLICATION
+                                ).toFixed(2)
+                            )}
+                        </Typography>
+                    )}
+                </>
+            )}
         </>
     );
 };
