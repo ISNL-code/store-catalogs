c07a120e9015eab944661c9b0e6a17ee33efbb1f
dev-cocktail <dmytro.orgish88@gmail.com>
2024-08-29 00:46:48 +0300

    wip

--- src/store_constants/stores_configs/RETAIL_ALBERTO_BINI.ts
+++ src/store_constants/stores_configs/RETAIL_ALBERTO_BINI.ts
@@ -55,7 +55,7 @@ export const RETAIL_ALBERTO_BINI = {
     web_head_data: {
         store_title: 'Alberto Bini Wholesales Catalog | Alberto Bini Оптовий Каталог',
         store_description: `Ласкаво просимо до Alberto Bini — світу вишуканого жіночого верхнього одягу! Дослідіть наш широкий вибір високоякісних стильних пальт, тренчів та курток. Погрузіться в унікальний стиль та комфорт з Alberto Bini!  Welcome to Alberto Bini — the world of exquisite women's outerwear! Explore our wide selection of high-quality stylish coats, trenches, and jackets. Immerse yourself in unique style and comfort with Alberto Bini!`,
-        google_analytics_id: 'G-C4TDRX7K5V',
+        google_analytics_id: 'G-SPMQE8GHGM',
         google_ads_id: '',
         conversion_id: '',
         store_logo: 'alberto_bini_logo.png',
