fa9674d6dbc41bb5eb1f8963332ab1f2e9260fbd
dev-cocktail <dmytro.orgish88@gmail.com>
2024-10-03 11:38:05 +0300

    wip

--- src/store_constants/stores_configs/ALBERTO_BINI_EU_WS.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_EU_WS.ts
@@ -2,8 +2,8 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const ALBERTO_BINI_EU_WS = {
     storage_key: 'ab_eu-ws',
-    active: false, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
-    status: 'Payment await', // показывает пока что в телеграме статус или причину почему каталог не работает
+    active: true, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
+    status: 'Очiкування оплати', // показывает пока что в телеграме статус или причину почему каталог не работает
     required_registration: false,
     URL: ['https://albertobini-brand.netlify.app'],
     web_lang: 'en', // html lang
@@ -33,6 +33,10 @@ export const ALBERTO_BINI_EU_WS = {
             {
                 token: '6904212535:AAGvPEjkJds0aayd-oD1YVMbhLKeKt72yaE',
                 chatId: '480774886',
+            },
+            {
+                token: '7501048516:AAFGRXP3nw7z0UWr4t-Dvz_oFt_98CFXX2k',
+                chatId: '6221489796',
             },
         ],
         product_image_options: { width: 6, height: 9 },
