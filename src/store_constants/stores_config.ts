1b06e6698a28028a8bde1b9d3af9f17be701f464
dev-cocktail <dmytro.orgish88@gmail.com>
2024-10-04 14:05:48 +0300

    wip

--- src/store_constants/stores_config.ts
+++ src/store_constants/stores_config.ts
@@ -1,5 +1,6 @@
 import { ALBERTO_BINI_EU_WS } from './stores_configs/ALBERTO_BINI_EU_WS';
 import { ALBERTO_BINI_KZ_EUR_WS } from './stores_configs/ALBERTO_BINI_KZ_EUR_WS';
+import { ALBERTO_BINI_KZ_WS } from './stores_configs/ALBERTO_BINI_KZ_WS';
 import { ALBERTO_BINI_MAIN } from './stores_configs/ALBERTO_BINI_MAIN';
 import { ALBERTO_BINI_SALES } from './stores_configs/ALBERTO_BINI_SALES';
 import { DEVELOP_STORE } from './stores_configs/A_DEVELOP_STORE';
@@ -19,6 +20,7 @@ const stores = [
     ROSALIE_ALBERTO_BINI,
     PASMA,
     ALBERTO_BINI_KZ_EUR_WS,
+    ALBERTO_BINI_KZ_WS,
 ];
 
 const storeConfig = (): STORE_CONFIG_Interface => {
