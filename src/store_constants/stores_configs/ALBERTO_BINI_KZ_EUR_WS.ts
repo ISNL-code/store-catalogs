2dcf8285a81b4766c888803f486f75868175e1d5
dev-cocktail <dmytro.orgish88@gmail.com>
2024-10-04 16:52:13 +0300

    wip

--- src/store_constants/stores_configs/ALBERTO_BINI_KZ_EUR_WS.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_KZ_EUR_WS.ts
@@ -2,7 +2,7 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const ALBERTO_BINI_KZ_EUR_WS = {
     storage_key: 'ab_kz-wssss',
-    active: true, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
+    active: false, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
     status: 'Очiкування оплати', // показывает пока что в телеграме статус или причину почему каталог не работает
     required_registration: false,
     URL: ['https://albertobini-kz.netlify.app'],
--- src/store_constants/stores_configs/ALBERTO_BINI_SALES.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_SALES.ts
@@ -2,7 +2,7 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const ALBERTO_BINI_SALES = {
     storage_key: 'ab_ua-wss',
-    active: true,
+    active: false,
     status: 'Технiчне обслуговування', // показывает пока что в телеграме статус или причину почему каталог не работает
     required_registration: false,
     URL: ['https://alberto-bini-sales.netlify.app'],
