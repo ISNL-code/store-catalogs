09a456704f246caca0e2a8341b72eea23e772444
dev-cocktail <dmytro.orgish88@gmail.com>
2024-10-06 15:14:16 +0300

    wip

--- src/layouts/WelcomeLayout/Welcome.tsx
+++ src/layouts/WelcomeLayout/Welcome.tsx
@@ -10,7 +10,7 @@ export default function WelcomePage({ handleRedirect }) {
 
     const redirectToStartPath = () => {
         const startPath = handleRedirect();
-        // navigate(startPath);
+        navigate(startPath);
     };
 
     setTimeout(redirectToStartPath, 3000);
@@ -62,13 +62,229 @@ export default function WelcomePage({ handleRedirect }) {
                     >
                         <img src={require('assets/img/logo.webp')} style={{ height: sx ? 70 : 100 }} alt="img" />
                     </Box>
-                    <Box>
-                        <Typography sx={{ fontSize: sx ? 30 : 48, fontWeight: 700, lineHeight: 1 }}>
-                            COCKTAIL
-                        </Typography>
-                        <Typography color="secondary" sx={{ fontSize: sx ? 34 : 54, fontWeight: 700, lineHeight: 1 }}>
-                            CATALOGS
-                        </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: 0.5,
+                            cursor: 'pointer',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                display: 'flex',
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 600,
+                                    // color: Color?.PRIMARY,
+                                    textShadow: '#0000006a 0 0 2px',
+                                    mr: 0.1,
+                                }}
+                            >
+                                C
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#268eca',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                o
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                C
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    // color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                K
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#f5cb56',
+                                    textShadow: '#000000 0 0 3.25px',
+                                }}
+                            >
+                                T
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    // color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                A
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                i
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                L
+                            </Typography>
+
+                            <Box
+                                sx={{
+                                    position: 'absolute',
+                                    bottom: 1,
+                                    left: 0,
+                                    borderTop: `2px solid ${'#268eca'}`,
+                                    width: '100%',
+                                    boxShadow: '0 0 2px #7c7c7c',
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    position: 'absolute',
+                                    bottom: -3,
+                                    left: 0,
+                                    borderTop: `2px solid ${'#f5cb56'}`,
+                                    width: '100%',
+                                    boxShadow: '0 0 1px  #7c7c7c6f',
+                                }}
+                            ></Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                display: 'flex',
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    ml: 1,
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 600,
+                                    // color: Color?.PRIMARY,
+                                    textShadow: '#0000006a 0 0 2px',
+                                    mr: 0.1,
+                                }}
+                            >
+                                C
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                A
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#f5cb56',
+                                    textShadow: '#000000 0 0 3.25px',
+                                }}
+                            >
+                                T
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                A
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    // color: Color?.PRIMARY,
+                                    textShadow: '#0000006a 0 0 2px',
+                                }}
+                            >
+                                L
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#268eca',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                o
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    // color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                G
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    fontFamily: 'Young Serif',
+                                    fontSize: sx ? 23 : 40,
+                                    fontWeight: 500,
+                                    color: '#2c4974',
+                                    textShadow: '#043f61 0 0 2.25px',
+                                }}
+                            >
+                                S
+                            </Typography>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
