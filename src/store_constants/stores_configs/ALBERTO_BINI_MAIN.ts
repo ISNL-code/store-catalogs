3615e366a747aeff236295564568332014ba60d0
dev-cocktail <dmytro.orgish88@gmail.com>
2024-11-01 19:52:43 +0200

    wip

--- src/store_constants/stores_configs/ALBERTO_BINI_MAIN.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_MAIN.ts
@@ -3,7 +3,7 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 export const ALBERTO_BINI_MAIN = {
     storage_key: 'ab_ua-ws',
     active: false, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
-    status: 'Компания Альберто Бини кидает людей на деньги',
+    status: 'Компания Альберто Бини кидает людей на деньгиб  предлагают сотрудничество а потом просто блокирует номер и не выплачивает долги',
     required_registration: false,
     URL: ['https://alberto-bini.netlify.app'],
     web_lang: 'uk', // html lang
