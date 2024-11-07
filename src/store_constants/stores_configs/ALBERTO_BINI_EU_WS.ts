b3ede2e27ac7001308dea27ca38142d0c3c86bc2
dev-cocktail <dmytro.orgish88@gmail.com>
2024-11-07 16:25:38 +0200

    wip

--- src/store_constants/stores_configs/ALBERTO_BINI_EU_WS.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_EU_WS.ts
@@ -2,8 +2,8 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const ALBERTO_BINI_EU_WS = {
     storage_key: 'ab_eu-ws',
-    active: false, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
-    status: 'Заборгованість по оплаті, поки власник шукає кошти — відвідайте наш сайт',
+    active: true, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
+    status: null,
     required_registration: false,
     URL: ['https://albertobini-brand.netlify.app'],
     web_lang: 'en', // html lang
--- src/store_constants/stores_configs/ALBERTO_BINI_KZ_WS.ts
+++ src/store_constants/stores_configs/ALBERTO_BINI_KZ_WS.ts
@@ -2,8 +2,8 @@ import { StoreType, ViewModeType } from 'store_constants/types';
 
 export const ALBERTO_BINI_KZ_WS = {
     storage_key: 'ab_kz-ws',
-    active: false, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
-    status: 'Заборгованість по оплаті, поки власник шукає кошти — відвідайте наш сайт',
+    active: true, // Поле которое запускает магазин true - работает false - остановлен / по тех. причина
+    status: null,
     required_registration: false,
     URL: ['https://albertobini-kz.com'],
     web_lang: 'kz', // html lang
