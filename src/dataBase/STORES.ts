4ecb9275f31cc979e3b05f4a953d8e2600434ae7
dev-cocktail <dmytro.orgish88@gmail.com>
2024-11-10 16:41:52 +0200

    wip

--- src/dataBase/STORES.ts
+++
@@ -1,9 +0,0 @@
-import { ALBERTO_BINI_EU_KZ_DB } from './stores_db/ALBERTO_BINI_EU_KZ_DB';
-import { ALBERTO_BINI_MAIN_DB } from './stores_db/ALBERTO_BINI_MAIN_DB';
-import { ALBERTO_BINI_OUTLET_DB } from './stores_db/ALBERTO_BINI_OUTLET_DB';
-
-export const STORES_DATA = [
-    ALBERTO_BINI_MAIN_DB, //main
-    ALBERTO_BINI_OUTLET_DB, // ws-sales, outlet,
-    ALBERTO_BINI_EU_KZ_DB, //eu, kz, ws, retail
-];
