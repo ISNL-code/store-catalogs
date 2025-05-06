ab775f463edac3c6a3f57cf5c27911db9eb96dd7
Dmytro Orgish <dmytro.orgish88@gmail.com>
2025-05-06 15:04:22 +0300

    wip

--- src/components/atoms/Logo/WelcomeLogo.tsx
+++ src/components/atoms/Logo/WelcomeLogo.tsx
@@ -10,6 +10,7 @@ const WelcomeLogo = () => {
             className="WelcomeLogo"
             sx={{
                 display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
                 gap: 1,
@@ -25,25 +26,24 @@ const WelcomeLogo = () => {
         >
             <Box
                 sx={{
-                    borderRadius: '50%',
+                    borderRadius: 2,
                     overflow: 'hidden',
-                    border: '1px solid #fff',
-                    width: sx ? 100 : 130,
-                    height: sx ? 100 : 130,
+                    border: '1px solid #ccc',
+                    width: sx ? 160 : 200,
+                    height: sx ? 100 : 140,
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                 }}
             >
-                <img src={require('assets/img/main_logo.webp')} style={{ height: sx ? 100 : 130 }} alt="img" />
+                <img src={require('assets/img/kremen.webp')} style={{ height: sx ? 120 : 160 }} alt="img" />
             </Box>
             <Box
                 sx={{
                     display: 'flex',
                     alignItems: 'center',
-                    gap: 0.5,
                     cursor: 'pointer',
-                    flexDirection: 'column',
+                    gap: 2,
                 }}
             >
                 <Box
@@ -54,111 +54,57 @@ const WelcomeLogo = () => {
                 >
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 600,
                             textShadow: '#0000006a 0 0 2px',
                             mr: 0.1,
                         }}
                     >
-                        C
+                        K
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                         }}
                     >
-                        o
+                        R
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        c
+                        E
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                         }}
                     >
-                        k
+                        M
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        t
+                        E
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        a
+                        N
                     </Typography>
-                    <Typography
-                        sx={{
-                            fontFamily: 'Young Serif',
-                            fontSize: sx ? 40 : 54,
-                            fontWeight: 500,
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    >
-                        i
-                    </Typography>
-                    <Typography
-                        sx={{
-                            fontFamily: 'Young Serif',
-                            fontSize: sx ? 40 : 54,
-                            fontWeight: 500,
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    >
-                        l
-                    </Typography>
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            bottom: 1,
-                            left: 0,
-                            borderTop: `4px solid ${'#268eca'}`,
-                            width: '100%',
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    ></Box>
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            bottom: -3,
-                            left: 0,
-                            borderTop: `3px solid ${'#f5cb56'}`,
-                            width: '100%',
-                            boxShadow: '0 0 1px  #7c7c7c6f',
-                        }}
-                    ></Box>
                 </Box>
                 <Box
                     sx={{
@@ -168,90 +114,48 @@ const WelcomeLogo = () => {
                 >
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 600,
                             textShadow: '#0000006a 0 0 2px',
                         }}
                     >
-                        C
+                        B
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        a
+                        E
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        t
+                        L
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        a
+                        T
                     </Typography>
                     <Typography
                         sx={{
-                            fontFamily: 'Young Serif',
                             fontSize: sx ? 40 : 54,
                             fontWeight: 500,
-                            color: '#3f5370',
                             textShadow: '#2c4974 0 0 2.25px',
                         }}
                     >
-                        l
-                    </Typography>
-                    <Typography
-                        sx={{
-                            fontFamily: 'Young Serif',
-                            fontSize: sx ? 40 : 54,
-                            fontWeight: 500,
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    >
-                        o
-                    </Typography>
-                    <Typography
-                        sx={{
-                            fontFamily: 'Young Serif',
-                            fontSize: sx ? 40 : 54,
-                            fontWeight: 500,
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    >
-                        g
-                    </Typography>
-                    <Typography
-                        sx={{
-                            fontFamily: 'Young Serif',
-                            fontSize: sx ? 40 : 54,
-                            fontWeight: 500,
-                            color: '#3f5370',
-                            textShadow: '#2c4974 0 0 2.25px',
-                        }}
-                    >
-                        s
+                        S
                     </Typography>
                 </Box>
             </Box>
